b4a319fce4d43c3373ea3563c0a8f89956228a17
Sander De la Marche <sander@sumocoders.be>
2022-03-25 11:03:10 +0100

    Use the new Page::setHTML method
    We can now drop the old method of dumping to a file, navigating
    to it and then generating a PDF from the visited page.

--- src/Service/PdfGenerator.php
+++ src/Service/PdfGenerator.php
@@ -5,25 +5,15 @@ declare(strict_types=1);
 namespace Dreadnip\ChromePdfBundle\Service;
 
 use HeadlessChromium\BrowserFactory;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 final class PdfGenerator
 {
     private BrowserFactory $browserFactory;
-    private RequestStack $requestStack;
-    private Filesystem $fileSystem;
-    private string $projectDir;
 
     public function __construct(
-        BrowserFactory $browserFactory,
-        RequestStack $requestStack,
-        string $projectDir
+        BrowserFactory $browserFactory
     ) {
         $this->browserFactory = $browserFactory;
-        $this->requestStack = $requestStack;
-        $this->fileSystem = new Filesystem();
-        $this->projectDir = $projectDir;
     }
 
     /**
@@ -38,39 +28,18 @@ final class PdfGenerator
      */
     public function generate(string $html, string $path, array $printOptions = [], array $browserOptions = []): string
     {
-        $relativeTempFilePath = 'tmp/' . bin2hex(random_bytes(32)) . '.html';
-
-        $absoluteTempFilePath = $this->projectDir . $relativeTempFilePath;
-
-        $this->fileSystem->dumpFile($absoluteTempFilePath, $html);
-
-        $tempUrl = $this->getUrl($relativeTempFilePath);
-
         $browser = $this->browserFactory->createBrowser($browserOptions);
 
         try {
             $page = $browser->createPage();
 
-            $page->navigate($tempUrl)->waitForNavigation();
+            $page->setHtml($html);
 
             $page->pdf($printOptions)->saveToFile($path, 300000);
-
-            $this->fileSystem->remove($this->projectDir . 'tmp');
 
             return $path;
         } finally {
             $browser->close();
         }
-    }
-
-    /**
-     * Return the absolute URL for our temporary dump file
-     *
-     * @param string $path
-     * @return string
-     */
-    private function getUrl(string $path): string
-    {
-        return $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost() . '/' . $path;
     }
 }
